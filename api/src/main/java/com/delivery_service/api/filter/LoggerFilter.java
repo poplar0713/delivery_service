26d1850381a6853b5fb3a9ccec9c7e6b1d78f739
Jeongdong <59277907+poplar0713@users.noreply.github.com>
2024-01-02 00:04:29 +0900

    Add response, request Log Filter

---
+++ api/src/main/java/com/delivery_service/api/filter/LoggerFilter.java
@@ -0,0 +1,61 @@
+package com.delivery_service.api.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class LoggerFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var req = new ContentCachingRequestWrapper((HttpServletRequest) servletRequest);
+        var res = new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
+
+        filterChain.doFilter(req, res);
+
+        // request 정보
+        var headerNames = req.getHeaderNames();
+        var headerValues = new StringBuilder();
+
+        headerNames.asIterator().forEachRemaining(headerKey ->{
+            var headerValue = req.getHeader(headerKey);
+            headerValues
+                    .append("[")
+                    .append(headerKey)
+                    .append(" : ")
+                    .append(headerValue)
+                    .append("] ");
+        });
+
+        var requestBody = new String(req.getContentAsByteArray());
+        var uri = req.getRequestURI();
+        var method = req.getMethod();
+
+        log.info(">>>> uri : {} , method : {} , header : {} , body : {}", uri, method, headerValues.toString(), requestBody);
+
+        //response 정보
+        var responseHeaderValues = new StringBuilder();
+        res.getHeaderNames().forEach(headerKey-> {
+            var headerValue = res.getHeader(headerKey);
+            responseHeaderValues
+                    .append("[")
+                    .append(headerKey)
+                    .append(" : ")
+                    .append(headerValue)
+                    .append("] ");
+        });
+
+        var responseBody = new String(res.getContentAsByteArray());
+        log.info("<<<< uri : {} , method : {} ,  header : {} , body : {}", uri, method, responseHeaderValues, responseBody);
+
+        res.copyBodyToResponse();
+    }
+}
